41c3d465f413911518f99b599804bcbcd77a9d04
sidgupta11 <86286322+sidgupta11@users.noreply.github.com>
2021-06-22 23:55:46 +0530

    test commit

--- src/test/java/net/finmath/equities/diffblue/Black76ModelDiffblueTest.java
+++ src/test/java/net/finmath/equities/diffblue/Black76ModelDiffblueTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class Black76ModelDiffblueTest {
 	@Test
 	public void testOptionPrice() {
